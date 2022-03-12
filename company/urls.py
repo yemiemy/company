1a7b1c001e99e309c1ab651bffb8908f8a566c15
Yemiemy <rasholayemi@gmail.com>
2022-03-13 00:13:54 +0100

    start project

---
+++ company/urls.py
@@ -0,0 +1,8 @@
+from core.views import home
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name="home")
+]
