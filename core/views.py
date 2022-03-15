f6ceb47ebd2d785f4579f43a87e1f7a07d33b146
The-KS101 <sesantakuro29@gmail.com>
2022-03-15 16:57:24 +0100

    Index init

--- core/views.py
+++ core/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, "templates/index.html")
+    return render(request, "index.html")
+
+def services(request):
+    return render(request, "services.html")
