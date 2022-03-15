517c42baf9dc803e1eaa7f1bb2863b90fc35c3c8
Yemiemy <rasholayemi@gmail.com>
2022-03-15 18:09:32 +0100

    updated views.py and urls.py

--- company/urls.py
+++ company/urls.py
@@ -1,8 +1,11 @@
-from core.views import home
+from core.views import home, contact, services, faq
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name="home")
+    path('', home, name="home"),
+    path('contact/', contact, name="contact"),
+    path('services/', services, name="services"),
+    path('faq/', faq, name="faq")
 ]
--- core/views.py
+++ core/views.py
@@ -7,3 +7,13 @@ def home(request):
 
 def services(request):
     return render(request, "services.html")
+    
+def contact(request):
+    return render(request, "contact.html")
+
+
+def faq(request):
+    return render(request, "faq.html")
+
+def page404(request):
+    return render(request, "404.html")
